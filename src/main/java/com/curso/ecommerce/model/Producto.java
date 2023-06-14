91339877d275717aa7a9e9ecbb24ca7a3d75c259
JhonatanOropeza <jhon_oropeza@hotmail.com>
2023-06-14 12:15:24 -0600

    Agregando la primer vista html del proyecto

--- src/main/java/com/curso/ecommerce/model/Producto.java
+++ src/main/java/com/curso/ecommerce/model/Producto.java
@@ -1,17 +1,33 @@
 package com.curso.ecommerce.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name= "productos")
 public class Producto {
+	@Id
+	@GeneratedValue (strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String nombre;
 	private String descripcion;
 	private String imagen;
 	private double precio;
-	private int cantidad;
+	private int cantidad;//Para la relación de las tablas->
 	
+	@ManyToOne
+	private Usuario usuario;
+		
 	public Producto() {
 		
 	}
-	public Producto(Integer id, String nombre, String descripcion, String imagen, double precio, int cantidad) {
+
+	public Producto(Integer id, String nombre, String descripcion, String imagen, double precio, int cantidad,
+			Usuario usuario) {
 		super();
 		this.id = id;
 		this.nombre = nombre;
@@ -19,7 +35,9 @@ public class Producto {
 		this.imagen = imagen;
 		this.precio = precio;
 		this.cantidad = cantidad;
+		this.usuario = usuario;
 	}
+
 	public Integer getId() {
 		return id;
 	}
@@ -56,7 +74,12 @@ public class Producto {
 	public void setCantidad(int cantidad) {
 		this.cantidad = cantidad;
 	}
-	
+	public Usuario getUsuario() {
+		return usuario;
+	}
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
 	@Override
 	public String toString() {
 		return "Producto [id=" + id + ", nombre=" + nombre + ", descripcion=" + descripcion + ", imagen=" + imagen
